879a01fe118273883c31ab5db23dee6f0588f4b4
LEONEL MERINO <46956927@ort>
2023-06-26 15:55:50 -0300

    logica terminada

--- src/AppBoton.jsx
+++ src/AppBoton.jsx
@@ -2,41 +2,50 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function AppBoton() {
-const [isLoading, setIsLoading] = useState(true);
-const [dogImage, setDogImage] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [image, setImage] = useState('');
+    const [name, setName] = useState('');
+    const [points, setPoints] = useState(0)
+    useEffect(() => {fetchImage();}, []);
 
-useEffect(() => {
-fetchDogImage();
-}, []);
+    const fetchImage = () => {
+        setIsLoading(true);
+        axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
+        .then((response) => {
+            let num = Math.round(Math.random() * 219);
+            setImage(response.data.data[num].flag);
+            setName(response.data.data[num].name);
+            setIsLoading(false);
+        })
+        .catch((error) => {console.log(error); setIsLoading(false);});
+    };
 
-const fetchDogImage = () => {
-setIsLoading(true);
-axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
-    .then((response) => {
-    let num = Math.round(Math.random() * 219);
-    setDogImage(response.data.data[num].flag);
-    setIsLoading(false);
-    })
-    .catch((error) => {
-    console.log(error);
-    setIsLoading(false);
-    });
-};
+    const submit = () => {
+        console.log(document.getElementById("input").value)
+        console.log (name);
+        if (document.getElementById("input").value === name) {
+            setPoints(points+10)
+        } else {
+            setPoints(points-1)
+        }
+        fetchImage()
+    };
 
+    if (isLoading) {
+    return (
+        <div className="App">
+        <h1>Cargando...</h1>
+        </div>
+    );
+    }
 
-if (isLoading) {
-return (
-    <div className="App">
-    <h1>Cargando...</h1>
-    </div>
-);
-}
+    return (
+        <div className="App">
 
-return (
-<div className="App">
-    <img src={dogImage} alt="Un perro" />
-    <p>{dogImage}</p>
-    <button onClick={fetchDogImage}>Cambiar imagen</button>
-</div>
-);
+            <img src={image} alt="Bandera" height={350}width={700} style={{ alignSelf: 'center' }}/>
+            <p>You have {points} points</p>
+            <input type="text" id="input" placeholder="Escriba el nombre del país aquí"></input>
+            <button  type="submit" onClick={submit}>Enviar</button>
+        </div>
+    );
 }
